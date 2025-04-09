d24ff44b126db869e42f2a2e0baf129d0b8cb181
Desorrallo 1 <desarrollo1@softoursistemas.com>
2025-04-09 10:03:19 +0200

    variables de entornos cambiadas

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   appName: 'MiApp Test',
-  apiBaseUrl: 'http://localhost:3000/plannerstats',
+  //apiBaseUrl: 'http://localhost:3000/plannerstats',
+  apiBaseUrl: 'https://api-dev.softoursistemas.org/plannerstats',
   oneSignal: {
     appId: 'ONESIGNAL-TEST-APP-ID',
     googleProjectNumber: '1111222233',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   appName: 'Plannerstats App',
-  apiBaseUrl: 'http://localhost:3000/plannerstats', // API local
+  //apiBaseUrl: 'http://localhost:3000/plannerstats', // API local
+  apiBaseUrl: 'https://api-dev.softoursistemas.org/plannerstats',
   oneSignal: {
     appId: 'ONESIGNAL-DEV-APP-ID', // ID de desarrollo
     googleProjectNumber: '1234567890', // Solo para Android
